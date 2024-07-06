7eb6ab2e5d8f6efed781844f0faccadbc27123b8
Samundeeswari Velusamy <sam.velusamy36@gmail.com>
2024-07-06 02:24:59 -0400

    @Controller @RequestMapping and @GetMapping

---
+++ src/main/java/com/perscholas/execercise_onerest/ExecerciseOneRestApplication.java
@@ -0,0 +1,13 @@
+package com.perscholas.execercise_onerest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExecerciseOneRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExecerciseOneRestApplication.class, args);
+    }
+
+}
